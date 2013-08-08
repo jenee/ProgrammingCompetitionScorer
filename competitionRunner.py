d127d9254aa6993d54fcef179a194ea0e545a26e
Jenee <jenee.hughes@gmail.com>
2013-08-07 18:41:02 -0700

    Modified the record files to have an file extension

--- competitionRunner.py
+++ competitionRunner.py
@@ -1,6 +1,6 @@
 import sys
 import time
-
+import os
 
 def printUsage():
    print "USAGE: python competitionRunner.py start <competitionName>"
@@ -30,9 +30,9 @@ if taskArg == "start":
    else:
       competitionName = str(competitionStart)
    # file io: http://www.tutorialspoint.com/python/python_files_io.htm
-   compRecordFile = open( competitionName, "w" )
+   compRecordFile = open( competitionName + ".rec", "w" )
 
-   compRecordFile.write( str(competitionStart) );
+   compRecordFile.write( str(competitionStart) +"\n" );
 
    compRecordFile.close();
 elif taskArg == "score":
@@ -40,19 +40,21 @@ elif taskArg == "score":
 elif taskArg.isdigit():
    #print "This will say that you've solved a problem!"
    if len(sys.argv) > 2:
-      competitionName = sys.argv[2]
+      competitionName = sys.argv[2]  + ".rec"
    else:
       printUsage()
       sys.exit()
    #print "in problem "+taskArg
    try:
-      if (os.path.exists(competitionName):
+      if os.path.exists(competitionName):
          compRecordFile = open(competitionName, "a")
+         compRecordFile.write( str(time.time())  +"\n");
+         compRecordFile.close();
+      else:
+         print "Competition file does not exist for \'"+competitionName+"\'!"
    except IOError:
       print "IO error when trying to access file "+ filename
-   finally:
-      compRecordFile.write( str(time.time()) );
-      compRecordFile.close();
+   
 else:
    printUsage()
 
